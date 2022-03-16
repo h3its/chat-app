f5e0803dd98c288012da16a94931e45642192d73
Heitmann <ericcheitmann@gmail.com>
2022-03-16 03:08:16 -0400

    added more files, calling it a night here, pick up adding css and go from there

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>...it was neither possible for necessary\nto educate people who never questioned anything</h1>');
+  res.sendFile('/index.html');
 });
 
 server.listen(3000, () => {
